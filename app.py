16b4aacd53e96d4e5b51d8f49e001b31bcefa88d
franscwa <g.francisco@knights.ucf.edu>
2022-12-22 15:22:21 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, request, redirect, url_for
+import pymysql
+import maps 
+
+app = Flask(__name__)
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='Indiana2022!',
+                             db='geoproject')
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_form():
+    if request.method == 'POST':
+        # Get the form data
+        location = request.form['location']
+
+
+
+        #add an image variable
+        title = request.form['title']
+        description = request.form['description']
+
+        # Insert the data into the database
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO landmarks (location, title, description) VALUES (%s, %s, %s)"
+            #also insert the data into the folium map
+            
+          
+
+
+            output = "SELECT * from landmarks"
+          
+         
+            cursor.execute(sql, (location, title, description))
+            cursor.execute(output)
+            print("HEY!!! " + output)
+        connection.commit()
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
