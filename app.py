7b7c25166276cd80b5771978aeb5c62fbd56c3aa
franscwa <g.francisco@knights.ucf.edu>
2023-01-01 21:24:16 -0500

    changed app.py file

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ from flask import Flask, request, redirect, url_for, render_template
 import pymysql
 from MapOOP import Maps
 from dotenv import load_dotenv
-
+import requests
 
 
 
 app = Flask(__name__)
 load_dotenv()
 
- #Connect to the database
 
-  
 dbHost = os.getenv("DATABASE_HOST")
 dbName = os.getenv("DATABASE_NAME")
 dbUser = os.getenv("DATABASE_USERNAME")
@@ -20,22 +18,21 @@ dbPass = os.getenv("DATABASE_PASSWORD")
 mapsAPI = os.getenv("MAP_API_KEY")
 
 
-
+#connect to the databse
 connection = pymysql.connect(host=dbHost,
                              user=dbUser,
                              password=dbPass,
                                 db=dbName)
 
 
-
+#get and post request on base '/' URL endpoint
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Get the form data
         city = request.form['location']
 
-       
-
+        #Google maps API to get latitude and longitude from city names
         response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address={city}&key={mapsAPI}')
 
         if response.status_code == 200:
@@ -52,7 +49,7 @@ def index():
         else:
             print('Request failed')
     
-
+        #the Maps object takes a list of coordinates as an input so we store/retrieve the latitude and longitude as a list
         location = []
 
         location.append(latitude)
@@ -65,7 +62,7 @@ def index():
         title = request.form['title']
         description = request.form['description']
 
-        # Insert the data into the database
+        # Insert the new landmark data into the database
         with connection.cursor() as cursor:
             sql = "INSERT INTO landmarks (location, title, description) VALUES (%s, %s, %s)"
            
@@ -97,7 +94,9 @@ def index():
 
 
     else: 
+        #if the request to the url is not a post request render the current/newly generated map 
         return render_template('index.html') 
+
         connection.commit()
         
 
